435625a5966d6b43edcb41f83ccf84145a0ed9dd
Umaira Khatri <135537802+Uma1ra@users.noreply.github.com>
2024-03-12 16:49:08 +0900

    [add] 演算子色々

---
+++ public/var14.php
@@ -0,0 +1,27 @@
+<?php
+$x = 5;
+$y = 2;
+$z = $x + $y; // 足し算
+
+echo $z;
+echo ",";
+
+$z = $x - $y; // 引き算
+echo $z;
+echo ",";
+
+$z = $x * $y; // 掛け算
+echo $z;
+echo ",";
+
+$z = $x / $y; // 割り算
+echo $z;
+echo ",";
+
+$z = $x % $y; // 余剰（割り算の余り）
+echo $z;
+echo ",";
+
+$z = $x ** $y; // 累乗（5の2乗）
+echo $z;
+echo ",";
