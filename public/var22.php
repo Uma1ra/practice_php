4aa72ffdfd66cef290d6ce0954f9bfe745dfa370
Umaira Khatri <135537802+Uma1ra@users.noreply.github.com>
2024-03-12 17:17:25 +0900

    [add] 比較演算子（3パターン） true→真 false→偽 !=→等しくない
    代入の=より他の演算記号のが処理が速い

--- public/var22.php
+++ public/var22.php
@@ -1,6 +1,17 @@
 <?php
+// false
+// $apple = 100;
+// $banana = 200;
+// $result = $apple > $banana;
+
+// true
+// $apple = 100;
+// $banana = 200;
+// $result = $apple < $banana;
+
+// true
 $apple = 100;
 $banana = 200;
-$result = $apple > $banana;
+$result = $apple != $banana;
 
 var_dump($result);
