ab765710891b237159dbfcf0fab06a4e39af6fac
Umaira Khatri <135537802+Uma1ra@users.noreply.github.com>
2024-03-12 15:48:35 +0900

    [add] 変数名での大文字小文字、混ぜる、キャメルケース、スネークケース 上記すべてphpで使用可能 小文字が一般的 キャメルケースかスネークケースに関しては、業務上統一

---
+++ public/var4.php
@@ -0,0 +1,13 @@
+<?php
+$name = "Andy"; // 一般的
+$Name = "Betty";
+$NAME = "Carol";
+
+$firstName = "CamelCase";
+$first_name = "snake_case";
+
+echo $name;
+echo $Name;
+echo $NAME;
+echo $firstName;
+echo $first_name;
