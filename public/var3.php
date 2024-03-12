179d29fe10e0e431b262ef324476a418f70cf645
Umaira Khatri <135537802+Uma1ra@users.noreply.github.com>
2024-03-12 15:53:58 +0900

    [fix] 例としての誤り構文がエラー表示されてしまうので→コメントアウト

--- public/var3.php
+++ public/var3.php
@@ -7,10 +7,10 @@
 // エラー文
 // PHP Parse error:  syntax error, unexpected integer "2", expecting variable or "{" or "$" in /var/www/practice_php/public/var3.php on line 6
 
-$name = "Andy";
-$name2 = "Betty";
-$all-names = $name . $name2;
+// $name = "Andy";
+// $name2 = "Betty";
+// $all-names = $name . $name2;
 // エラー文
 // PHP Parse error:  syntax error, unexpected token "=" in /var/www/practice_php/public/var3.php on line 12
 
-echo $all-names;
+// echo $all-names;
