c4745385a86ab43d391852c86f6c3188a2556d64
Umaira Khatri <135537802+Uma1ra@users.noreply.github.com>
2024-03-14 12:17:46 +0900

    [update] 空白追加（自己満）

--- public/choice/choice4.php
+++ public/choice/choice4.php
@@ -2,7 +2,7 @@
 $diceA = 4;
 $diceB = 3;
 
-if ($diceA >= 4 && $diceB >=4) { // かつ（どちらの条件成立しないといけない）
+if ($diceA >= 4 && $diceB >= 4) { // かつ（どちらの条件成立しないといけない）
     echo "Win";
 } else {
     echo "Lose";
