3dd19e55b31171df25136f83716abe655a359a78
Umaira Khatri <135537802+Uma1ra@users.noreply.github.com>
2024-03-14 14:45:05 +0900

    [update] foreachメソッドを使用し、足し算を繰り返して合算

--- public/associative_array/a_array_tr1.php
+++ public/associative_array/a_array_tr1.php
@@ -1,6 +1,12 @@
 <?php
 $score = ["english" => 90, "math" => 88, "science" => 80];
 // $total_score = array_sum($score); // 関数のが簡単だけど演習の意味がなくなるので
-$total_score = $score["english"] + $score["math"] + $score["science"];
 
-echo $total_score;
+// $total_score = $score["english"] + $score["math"] + $score["science"];
+// echo $total_score;
+
+$total = 0;
+foreach ($score as $key => $value) {
+    $total += $value;
+}
+echo $total . PHP_EOL;
