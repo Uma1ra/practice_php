61683d3488672156db99163ebd72be47fc4eee3a
Umaira Khatri <135537802+Uma1ra@users.noreply.github.com>
2024-03-15 17:31:16 +0900

    [update] 既にPHPに使える関数がある場合は使おう！

---
+++ public/function/function_tr2.php
@@ -0,0 +1,15 @@
+<?php
+// function repeat($input, $times) {
+//     $result = "";
+//     for ($i = 0; $i < $times; $i++) { 
+//         $result .= $input;
+//     }
+
+//     return $result;
+// }
+
+$hello2 = str_repeat("Hello", 2);
+echo $hello2 . PHP_EOL;
+
+$hello3 = str_repeat("World", 3);
+echo $hello3 . PHP_EOL;
