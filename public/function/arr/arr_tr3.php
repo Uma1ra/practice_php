912a4aaeb3d3b4988488d1260276bc790b5aba2b
Umaira Khatri <135537802+Uma1ra@users.noreply.github.com>
2024-03-15 12:12:10 +0900

    [add] 演習3（配列要素を逆降順） sort() + array_reverse() = rsort()

---
+++ public/function/arr/arr_tr3.php
@@ -0,0 +1,10 @@
+<?php
+$names = ["Carol", "Andy", "Betty", "Daniel"];
+
+// sort($names); // アルファベット順に
+// $reversed = array_reverse($names); // 順番を逆に
+// var_dump($reversed);
+
+rsort($names); // reverse sort、逆アルファベット順
+var_dump($names);
+
