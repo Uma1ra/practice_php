a035069da9894d6422d6a5cd5ed7cdf85fbb4055
Umaira Khatri <135537802+Uma1ra@users.noreply.github.com>
2024-03-21 16:03:05 +0900

    [add] HTML内にPHPを埋め込んで表示

---
+++ public/web/web_tr1.php
@@ -0,0 +1,15 @@
+<?php
+$date = date("Y-m-d H:i:s");
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP_web_tr1</title>
+</head>
+<body>
+    <h1>Date: <?php echo $date; ?></h1>
+</body>
+</html>
