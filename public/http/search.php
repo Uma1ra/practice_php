5ad9a5645cb2a762a8244b0023e8010dbea37e1a
Umaira Khatri <135537802+Uma1ra@users.noreply.github.com>
2024-03-21 17:34:20 +0900

    [add] htmlとphpを使用して検索フォームの作成

---
+++ public/http/search.php
@@ -0,0 +1,34 @@
+<?php
+$name = $_GET["name"];
+$names = file("names.txt", FILE_IGNORE_NEW_LINES);
+
+$searched_names = [];
+if ($name !== "") {
+    for ($i = 0; $i < count($names); $i++) { 
+        if (strpos($names[$i], $name) !== false) {
+            $searched_names[] = $names[$i];
+        }
+    }
+}
+?>
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP_http_search</title>
+</head>
+<body>
+    <h3>Search</h3>
+    <hr>
+    <ul>
+        <?php
+        for ($i = 0; $i < count($searched_names); $i++) {
+        ?>
+        <li><?php echo $searched_names[$i]; ?></li>
+        <?php    
+        }
+        ?>
+    </ul>
+</body>
+</html>
