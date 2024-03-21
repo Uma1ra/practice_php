e9d13b084ed612cebe7f5879503f906d02d77c9a
Umaira Khatri <135537802+Uma1ra@users.noreply.github.com>
2024-03-21 16:52:46 +0900

    [add] phpとhtmlでログインフォームを作成 $_POST、パラメータはボディパート内で送信される ブラウザのアドレスバーに表示されない
    $_GET、パラメータは（クエリパラメータとして）URLに含まれて送信される
    ブラウザのアドレスバーに表示される、ボディパートは使用されない

---
+++ public/http/login.php
@@ -0,0 +1,21 @@
+<?php
+$id = $_POST["id"];
+$password = $_POST["password"];
+$message = "NG";
+
+if ($id === "Andy" && $password === "secret") {
+    $message ="OK";
+}
+?>
+
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP</title>
+</head>
+<body>
+    <h1>Login <?php echo $message; ?></h1>
+</body>
+</html>
